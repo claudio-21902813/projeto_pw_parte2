6825d1e8f660f02b652542a3bf09bd9296e3f3aa
Nuno Caetano <a21902813@alunos.ulht.pt>
2021-05-18 17:26:51 +0100

    projeto

--- tec_noticias/urls.py
+++ tec_noticias/urls.py
@@ -7,11 +7,11 @@ app_name = "tec_noticias"
 
 urlpatterns = [
     path('', views.home_page_view, name='home'),
-    path('about', views.about_page_view, name='about'),
     path('info', views.info_page_view, name='info'),
     path('quizz',views.quizz_view,name='quizz'),
     path('contacto', views.contact_page_view, name='contacto'),
     path('apple:noticias',views.apple_page_view,name='apple'),
     path('windows:noticias',views.windows_page_view,name='windows'),
     path('linux:noticias',views.linux_page_view,name='linux'),
+    path('about', views.about_page_view, name='about'),
 ]
