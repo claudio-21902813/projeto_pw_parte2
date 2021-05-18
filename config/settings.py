a8e73fe879c657c8c2e2046172b27e52deacdfbb
Nuno Caetano <a21902813@alunos.ulht.pt>
2021-05-18 18:10:28 +0100

    projeto django

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^utb&)i@e9=_zjpm#m5yb6oz(6jb4nw1)xy@mi%4k87b&#lx2q'
 #SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = env.bool("DEBUG", default=True)
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
