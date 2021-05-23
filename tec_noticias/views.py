c5959b7afe5e84afab1dc73b7d8a1a1ffc7b928b
Nuno Caetano <a21902813@alunos.ulht.pt>
2021-05-23 17:52:51 +0100

    projeto django

--- tec_noticias/views.py
+++ tec_noticias/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-import matplotlib.pyplot as plt # Graficos
-
+import matplotlib.pyplot as plt
 # Create your views here.
 from django.urls import reverse
 
@@ -325,7 +324,9 @@ def quizz_resultado(request,id):
         lista_pontos.append(0)
 
 
-    plt.bar(['p1','p2','p3','p4','p5','p7','p8','p9','p10'],lista_pontos)
+    #plt.bar(['p1','p2','p3','p4','p5','p7','p8','p9','p10'],lista_pontos)
+    plt.plot([1, 2, 3, 4])
+    plt.ylabel('some numbers')
     plt.savefig('tec_noticias/static/website/imagens/plot.png',bbox_inches='tight')
 
 
